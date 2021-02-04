fd0003b95f3b5018e9d96693f8a8b99e06b9baa5
Jeremy Hall <21281011+jeremysb1@users.noreply.github.com>
2021-02-04 14:14:51 -0500

    asynchronous test w/api call

---
+++ script2.js
@@ -0,0 +1,17 @@
+// Asynchronous tests
+
+const fetch = require('node-fetch');
+
+const getPeople = fetch => {
+	return fetch('https://swapi.dev/api/people')
+	  .then(response => response.json())
+	  .then(data => {
+	  	console.log(data);
+	  	return {
+	  		count: data.count,
+	  		results: data.results
+	  	}
+	  })
+}
+
+getPeople(fetch);
